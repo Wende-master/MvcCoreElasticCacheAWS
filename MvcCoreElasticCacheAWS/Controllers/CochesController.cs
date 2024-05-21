d9373cdfc5e6c127d91cfe4b3c6381bb276519fe
Wende-master <wendemu.lopez@tajamar365.com>
2024-05-21 13:25:27 +0200

    Add project files.

---
+++ MvcCoreElasticCacheAWS/Controllers/CochesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreElasticCacheAWS.Models;
+using MvcCoreElasticCacheAWS.Repositories;
+
+namespace MvcCoreElasticCacheAWS.Controllers
+{
+    public class CochesController : Controller
+    {
+        private RepositoryCoches repo;
+        public CochesController(RepositoryCoches repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Coche> coches =
+                this.repo.GetCoches();
+            return View(coches);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Coche coche = this.repo.FindCoche(id);
+            return View(coche);
+        }
+    }
+}
---
+++ MvcCoreElasticCacheAWS/Helpoers/HelperCacheRedis.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElasticCacheAWS.Helpoers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer>
+            CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                //AQUÍ VA LA CADENA DE CONEXIÓN 
+                return ConnectionMultiplexer.Connect("");
+            });
+
+        public ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
